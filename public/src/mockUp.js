dcbf67a24b488c9f8f5af5f7de088a6fb445bd6c
George Vazquez <george.h.vazquez@gmail.com>
2023-09-16 18:15:32 -0400

    styling setup

--- public/src/mockUp.js
+++ public/src/mockUp.js
@@ -1,13 +1,21 @@
-let div = document.createElement('div');
-div.append('some text');
-div.className = 'mockContainer';
+const container = document.createElement('div');
+container.className = 'mockContainer';
+let mock = `
+    <div class="imageContainer">
+      <img src='public/assets/left.svg'>
+      <img class="mockImage" src=public/assets/Zukeeper-1.gif>
+      <img src='public/assets/right.svg'>
+    </div>
+    <img src='public/assets/exit.svg'>
+  `
 
 const addMockDiv = () => {
-  document.body.appendChild(div);
+  container.innerHTML = mock;
+  document.body.appendChild(container);
 };
 
 const removeMockDiv = () => {
-  document.body.removeChild(div);
+  document.body.removeChild(container);
 };
 
 export default { addMockDiv, removeMockDiv };
